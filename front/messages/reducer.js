a79e59d4b3431bf45e7c37d8ad91b90914393a05
Ralph Sleigh <ralph.sleigh@gmail.com>
2017-03-02 13:43:35 +0000

    fixing bugs from whitespace reformat??

--- front/messages/reducer.js
+++ front/messages/reducer.js
@@ -6,8 +6,8 @@ const initalMessageState = Immutable.Map();
 
 export default function Messages(state = initalMessageState, action) {
 	switch (action.type) {
-		case a.SET_WARNING_MESSAGE: return state.set("warning", { message: action.message }).set("success", null);
-		case a.SET_SUCCESS_MESSAGE: return state.set("success", { message: action.message }).set("warning", null);
+		case a.SET_WARNING_MESSAGE: return state.set("warning", { message: action.message, time: new Date() }).set("success", null);
+		case a.SET_SUCCESS_MESSAGE: return state.set("success", { message: action.message, time: new Date() }).set("warning", null);
 	}
 	return state;
 
