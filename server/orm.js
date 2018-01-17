964bc923e18c0559e0fbc22fb6ba8940da342e9e
Ralph Sleigh <ralph.sleigh@gmail.com>
2018-01-17 18:24:35 +0000

    Finished updating seed script

--- server/orm.js
+++ server/orm.js
@@ -1,35 +1,37 @@
+module.exports = (async () => {
 const Sequelize = require('sequelize');
 const fs = require('fs');
 const path = require('path');
+    const config = await require("../config");
 
 
-const sequelize = new Sequelize('bookings', null, null, {
-	dialect: 'sqlite', storage: 'database.sqlite',
-    logging: console.log,
-    operatorsAliases: Sequelize.Op.Aliases
+    const sequelize = new Sequelize(config.databaseURL, {
+        operatorsAliases: Sequelize.Op.Aliases
 });
 
-const db = {};
+    const db = {};
 const modelsDir = path.join(__dirname, 'models');
 
 
-fs
-	.readdirSync(modelsDir)
-	.filter(file => {
-		return (file.indexOf('.') !== 0);
-	})
-	.forEach(file => {
-        const model = sequelize['import'](path.join(modelsDir, file));
-		db[model.name] = model;
-	});
+    fs
+        .readdirSync(modelsDir)
+        .filter(file => {
+            return (file.indexOf('.') !== 0);
+        })
+        .forEach(file => {
+            const model = sequelize['import'](path.join(modelsDir, file));
+            db[model.name] = model;
+        });
 
-Object.keys(db).forEach(modelName => {
-	if (db[modelName].associate) {
-		db[modelName].associate(db);
-	}
+    Object.keys(db).forEach(modelName => {
+        if (db[modelName].associate) {
+            db[modelName].associate(db);
+        }
 });
 
-db.sequelize = sequelize;
+    db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-module.exports = db;
+    return db;
+
+})();
