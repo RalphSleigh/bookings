4c8d12b79e1c20368942f679491a557d6b71afed
Ralph Sleigh <ralph.sleigh@gmail.com>
2018-01-22 17:41:44 +0000

    Logging

--- server/logging.js
+++ server/logging.js
@@ -1,9 +1,23 @@
-var winston = require('winston');
+const {createLogger, format, transports} = require('winston');
 
-var logger = new winston.Logger({level:"debug"});
+const alignedWithColorsAndTime = format.combine(
+    format.colorize(),
+    format.timestamp(),
+    format.align(),
+    format.printf((info) => {
+        const {
+            timestamp, level, message, ...args
+        } = info;
 
+        const ts = timestamp.slice(0, 19).replace('T', ' ');
+        return `${ts} [${level}]: ${message} ${Object.keys(args).length ? JSON.stringify(args, null, 2) : ''}`;
+    }),
+);
 
-logger.add(winston.transports.Console, {colorize: true, prettyPrint: true});
+const logger = createLogger({
+    format: alignedWithColorsAndTime,
+    transports: [new transports.Console()]
+});
 
 logger.info("Logging configurted");
 
