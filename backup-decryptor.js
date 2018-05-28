4321bc45cef417167ec8a1fad76a9eea8b9abad6
Ralph Sleigh <ralph.sleigh@gmail.com>
2018-05-28 14:10:06 +0100

    oh jeeze

--- backup-decryptor.js
+++ backup-decryptor.js
@@ -3,11 +3,11 @@ const secureStreams = require('node-secure-stream');
 
 const key = fs.readFileSync('key.pem');
 const enc = new secureStreams.Decrypter({key: key});
-const file = fs.createReadStream(process.argv[1]);
+const file = fs.createReadStream(process.argv[2]);
 
 
 file.pipe(enc);
 
-const writable = fs.createWriteStream(process.argv[2]);
+const writable = fs.createWriteStream(process.argv[3]);
 
 enc.pipe(writable);
