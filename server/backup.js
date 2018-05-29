af2fe359bc961879bc7bcf9360fb24d550b77043
Ralph Sleigh <ralph.sleigh@gmail.com>
2018-05-29 19:23:41 +0100

    Try upload

--- server/backup.js
+++ server/backup.js
@@ -41,7 +41,7 @@ function doBackup() {
         Body: enc
     };
 
-    s3.putObject(params, function (err, data) {
+    s3.upload(params, function (err, data) {
         if (err) console.log(err, err.stack); // an error occurred
         else console.log(data);           // successful response
         /*
