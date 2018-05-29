4a95a97d1acd76e58aa62e3888c3ce7560a38d02
Ralph Sleigh <ralph.sleigh@gmail.com>
2018-05-29 19:43:18 +0100

    Schedule job

--- server/backup.js
+++ server/backup.js
@@ -6,6 +6,7 @@ const {spawn} = require('child_process');
 const crypto = require('crypto');
 const fs = require('fs');
 const secureStreams = require('node-secure-stream');
+var schedule = require('node-schedule');
 
 const dbURL = new url(config.DB_URL);
 
@@ -15,42 +16,42 @@ if (dbURL.protocol !== 'postgres:') {
 }
 
 function doBackup() {
-    const process = spawn('pg_dump', [config.DB_URL]);
+    try {
+        const process = spawn('pg_dump', [config.DB_URL]);
 
-    process.stderr.on('data', (data) => {
-        log.error(`Backup Error: ${data}`);
-    });
+        process.stderr.on('data', (data) => {
+            log.error(`Backup Error: ${data}`);
+        });
 
-    const enc = new secureStreams.Encrypter({public_key: config.BACKUP_PUBLIC_KEY});
+        const enc = new secureStreams.Encrypter({public_key: config.BACKUP_PUBLIC_KEY});
 
-    process.stdout.pipe(enc);
+        process.stdout.pipe(enc);
 
-    const AWS = require('aws-sdk');
-    AWS.config.update({
-        region: 'eu-west-2',
-        accessKeyId: config.AWS_BACKUP_KEY,
-        secretAccessKey: config.AWS_BACKUP_SECRET
-    });
-    const s3 = new AWS.S3();
+        const AWS = require('aws-sdk');
+        AWS.config.update({
+            region: 'eu-west-2',
+            accessKeyId: config.AWS_BACKUP_KEY,
+            secretAccessKey: config.AWS_BACKUP_SECRET
+        });
+        const s3 = new AWS.S3();
 
-    const key = `${config.AWS_BACKUP_PATH}/${Math.floor(new Date() / 1000)}.sql.enc`;
+        const key = `${config.AWS_BACKUP_PATH}/${Math.floor(new Date() / 1000)}.sql.enc`;
 
-    const params = {
-        Bucket: config.AWS_BACKUP_BUCKET,
-        Key: key,
-        Body: enc
-    };
+        const params = {
+            Bucket: config.AWS_BACKUP_BUCKET,
+            Key: key,
+            Body: enc
+        };
 
-    s3.upload(params, function (err, data) {
-        if (err) console.log(err, err.stack); // an error occurred
-        else console.log(data);           // successful response
-        /*
-        data = {
-         ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"",
-         VersionId: "pSKidl4pHBiNwukdbcPXAIs.sshFFOc0"
-        }
-        */
-    });
+        s3.upload(params, function (err, data) {
+            if (err) throw err; // an error occurred
+            else log.info("Backup Successful");
+        });
+
+    } catch (e) {
+        log.error(e);
+    }
 }
 
 doBackup();
+var j = schedule.scheduleJob('* 1 * * *', doBackup);
