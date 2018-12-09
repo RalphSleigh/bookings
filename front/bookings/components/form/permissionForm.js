2d4dc32a4904e70892cd7e53248fcc9a8341c577
Ralph Sleigh <ralph.sleigh@gmail.com>
2018-12-09 13:18:01 +0000

    Update permission text

--- front/bookings/components/form/permissionForm.js
+++ front/bookings/components/form/permissionForm.js
@@ -139,13 +139,14 @@ export default class PermissionForm extends React.Component {
                         <Label check>
                             <Input type="checkbox" checked={!!this.props.permission}
                                    onChange={this.updatePermission}/>{' '}
-                            I give permission for the people named above to
+                            I have permission for the people named above to
                             attend {this.props.event.name}.<br/><br/>
-                            I acknowledge it is my responsibility to ensure everyone over 16 attending has up-to-date
-                            Woodcraft Folk membership and completed a DBS check.
+                            I acknowledge it is my responsibility to ensure everyone over 16 attending is a member of an
+                            IFM-SEI organisation including Woodcraft Folk.
                             <br/><br/>
                             I agree this information will be stored electronically and shared only with individuals who need this information to
-                            engage your child safely in Woodcraft Folk activities. Based on the needs of your child we may also share any relevant
+                            engage each participant safely in Woodcraft Folk activities. Based on the needs of each
+                            participant we may also share any relevant
                             information with medical or child protection professionals. For more information please visit
                             <a href="https://woodcraft.org.uk/privacy"> www.woodcraft.org.uk/privacy</a> or contact <a
                             href="mailto:data@woodcraft.org.uk">data@woodcraft.org.uk</a>
