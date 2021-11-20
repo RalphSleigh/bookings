e8f4f5cdf0dc5c21a5a554771358ce474b484cff
RalphSleighC4 <ralph.sleigh@auticon.co.uk>
2021-11-20 14:26:56 +0000

    Add seperate DBS button

--- front/manage/components/membership.js
+++ front/manage/components/membership.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 //import bookings from '../bookings'
 //import { manageEventCheck } from '../permission.js'
@@ -22,13 +22,15 @@ export default class Memberships extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {copied: 0};
+        this.state = { copied: 0 };
 
         this.ageWidgets = ageFactory(this.props.Event.toJS());
 
         this.approveMembership = this.approveMembership.bind(this);
         this.unapproveMembership = this.unapproveMembership.bind(this);
 
+        this.approveDBS = this.approveDBS.bind(this);
+        this.unapproveDBS = this.unapproveDBS.bind(this);
     }
 
     shouldComponentUpdate(nextProps, nextState) {
@@ -41,7 +43,7 @@ export default class Memberships extends React.Component {
     componentWillReceiveProps() {
     }
 
-    approveMembership(id){
+    approveMembership(id) {
         return e => {
             this.props.approveMembership({
                 id: id
@@ -50,7 +52,7 @@ export default class Memberships extends React.Component {
         }
     }
 
-    unapproveMembership(id){
+    unapproveMembership(id) {
         return e => {
             this.props.unapproveMembership({
                 id: id
@@ -58,6 +60,25 @@ export default class Memberships extends React.Component {
             e.preventDefault();
         }
     }
+
+    approveDBS(id) {
+        return e => {
+            this.props.approveDBS({
+                id: id
+            });
+            e.preventDefault();
+        }
+    }
+
+    unapproveDBS(id) {
+        return e => {
+            this.props.unapproveDBS({
+                id: id
+            });
+            e.preventDefault();
+        }
+    }
+
 
     render() {
 
@@ -71,22 +92,45 @@ export default class Memberships extends React.Component {
 
         adults.sort(propSort('district'));
 
-        const approved = adults.filter(p => p.internalExtra.member === true);
-        const notYetApproved = adults.filter(p => p.internalExtra.member !== true);
+        const approvedBoth = adults.filter(p => p.internalExtra.member === true && p.internalExtra.dbs === true);
+        const approvedDBSOnly = adults.filter(p => p.internalExtra.member !== true && p.internalExtra.dbs === true);
+        const approvedMembershipOnly = adults.filter(p => p.internalExtra.member === true && p.internalExtra.dbs !== true);
+        const notYetApproved = adults.filter(p => p.internalExtra.member !== true && p.internalExtra.dbs !== true);
 
         const notYetApprovedRows = notYetApproved.map(p => <tr key={p.id}>
-            <td>{p.name + (p.ageAtStart === 16 ? '  (16)': '')}</td>
+            <td>{p.name + (p.ageAtStart === 16 ? '  (16)' : '')}</td>
             <td>{p.district}</td>
             <td>{p.externalExtra.adultEmail} <CopyToClipboard text={p.externalExtra.adultEmail}
-                onCopy={() => this.setState({copied: p.id})}><span style={{cursor:'copy'}}>📋</span></CopyToClipboard>{this.state.copied === p.id ?' ✔️' : ''}</td>
-            <td><Button color="success" onClick={this.approveMembership(p.id)}>Approve</Button></td>
+                onCopy={() => this.setState({ copied: p.id })}><span style={{ cursor: 'copy' }}>📋</span></CopyToClipboard>{this.state.copied === p.id ? ' ✔️' : ''}</td>
+            <td><Button color="success" onClick={this.approveMembership(p.id)}>Approve Membership</Button></td>
+            <td><Button color="success" onClick={this.approveDBS(p.id)}>Approve DBS</Button></td>
         </tr>);
 
-        const approvedRows = approved.map(p => <tr key={p.id}>
-            <td>{p.name + (p.ageAtStart === 16 ? '  (16)': '')}</td>
+        const approvedDBSOnlyRows = approvedDBSOnly.map(p => <tr key={p.id}>
+            <td>{p.name + (p.ageAtStart === 16 ? '  (16)' : '')}</td>
             <td>{p.district}</td>
-            <td>{p.externalExtra.adultEmail} <CopyToClipboard text={p.externalExtra.adultEmail}><span style={{cursor:'copy'}}>📋</span></CopyToClipboard>{this.state.copied === p.id ?' ✔️' : ''}</td>
-            <td><Button color="warning" onClick={this.unapproveMembership(p.id)}>Unapprove</Button></td>
+            <td>{p.externalExtra.adultEmail} <CopyToClipboard text={p.externalExtra.adultEmail}
+                onCopy={() => this.setState({ copied: p.id })}><span style={{ cursor: 'copy' }}>📋</span></CopyToClipboard>{this.state.copied === p.id ? ' ✔️' : ''}</td>
+            <td><Button color="success" onClick={this.approveMembership(p.id)}>Approve Membership</Button></td>
+            <td><Button color="warning" onClick={this.unapproveDBS(p.id)}>Unapprove DBS</Button></td>
+        </tr>);
+
+        const approvedMembershipOnlyRows = approvedMembershipOnly.map(p => <tr key={p.id}>
+            <td>{p.name + (p.ageAtStart === 16 ? '  (16)' : '')}</td>
+            <td>{p.district}</td>
+            <td>{p.externalExtra.adultEmail} <CopyToClipboard text={p.externalExtra.adultEmail}
+                onCopy={() => this.setState({ copied: p.id })}><span style={{ cursor: 'copy' }}>📋</span></CopyToClipboard>{this.state.copied === p.id ? ' ✔️' : ''}</td>
+            <td><Button color="warning" onClick={this.unapproveMembership(p.id)}>Unapprove Membership</Button></td>
+            <td><Button color="success" onClick={this.approveDBS(p.id)}>Approve DBS</Button></td>
+        </tr>);
+
+        const approvedBothRows = approvedBoth.map(p => <tr key={p.id}>
+            <td>{p.name + (p.ageAtStart === 16 ? '  (16)' : '')}</td>
+            <td>{p.district}</td>
+            <td>{p.externalExtra.adultEmail} <CopyToClipboard text={p.externalExtra.adultEmail}
+                onCopy={() => this.setState({ copied: p.id })}><span style={{ cursor: 'copy' }}>📋</span></CopyToClipboard>{this.state.copied === p.id ? ' ✔️' : ''}</td>
+            <td><Button color="warning" onClick={this.unapproveMembership(p.id)}>Unapprove Membership</Button></td>
+            <td><Button color="warning" onClick={this.unapproveDBS(p.id)}>Unapprove DBS</Button></td>
         </tr>);
 
         return (<Row>
@@ -94,15 +138,50 @@ export default class Memberships extends React.Component {
                 <h4>To Be Approved:</h4>
                 <table className='table'>
                     <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>District</th>
-                        <th>Membership e-mail</th>
-                        <th>Approve</th>
-                    </tr>
+                        <tr>
+                            <th>Name</th>
+                            <th>District</th>
+                            <th>Membership e-mail</th>
+                            <th>Membership</th>
+                            <th>DBS</th>
+                        </tr>
                     </thead>
                     <tbody>
-                    {notYetApprovedRows}
+                        {notYetApprovedRows}
+                    </tbody>
+                </table>
+            </Col>
+            <Col sm={12}>
+                <h4>Membership Approved:</h4>
+                <table className='table'>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>District</th>
+                            <th>Membership e-mail</th>
+                            <th>Membership</th>
+                            <th>DBS</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {approvedMembershipOnlyRows}
+                    </tbody>
+                </table>
+            </Col>
+            <Col sm={12}>
+                <h4>DBS Approved:</h4>
+                <table className='table'>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>District</th>
+                            <th>Membership e-mail</th>
+                            <th>Membership</th>
+                            <th>DBS</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {approvedDBSOnlyRows}
                     </tbody>
                 </table>
             </Col>
@@ -110,15 +189,16 @@ export default class Memberships extends React.Component {
                 <h4>Already Approved:</h4>
                 <table className='table'>
                     <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>District</th>
-                        <th>Membership e-mail</th>
-                        <th>Unapprove</th>
-                    </tr>
+                        <tr>
+                            <th>Name</th>
+                            <th>District</th>
+                            <th>Membership e-mail</th>
+                            <th>Membership</th>
+                            <th>DBS</th>
+                        </tr>
                     </thead>
                     <tbody>
-                    {approvedRows}
+                        {approvedBothRows}
                     </tbody>
                 </table>
             </Col>
@@ -126,7 +206,7 @@ export default class Memberships extends React.Component {
     }
 }
 
-const propSort = prop => (a,b) => {
+const propSort = prop => (a, b) => {
     const x = a[prop].toLowerCase();
     const y = b[prop].toLowerCase();
     return x < y ? -1 : x > y ? 1 : 0;
