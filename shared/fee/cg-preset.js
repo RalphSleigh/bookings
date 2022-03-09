0166ae68e3f56fbbf8896686b052f21c292758e9
Ralph Sleigh <ralph.sleigh@gmail.com>
2022-03-09 17:56:50 +0000

    Fix this

--- shared/fee/cg-preset.js
+++ shared/fee/cg-preset.js
@@ -438,7 +438,7 @@ const linesWithPartial = (combined, booking) => reduce(combined, (a, c, i) => [.
 
 const cancelledFee = (event, participants, booking) => {
     let maxToUse
-    if(moment().isBefore(booking.event.bookingDeadline)) {
+    if(moment().isBefore(booking.bookingDeadline)) {
         maxToUse = participants.length
     } else {
         maxToUse = Math.max(booking.maxParticipants || 0, participants.length);
