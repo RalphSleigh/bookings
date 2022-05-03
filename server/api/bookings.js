04f35945b6256cb2a52d66296d95a229c7773fde
Ralph <ralph.sleigh@gmail.com>
2022-05-03 15:51:25 +0100

    Revert "try only 20"
    This reverts commit d873bc872d66528971b6d2c6bca793b4ab0ea1f5.

--- server/api/bookings.js
+++ server/api/bookings.js
@@ -68,7 +68,7 @@ bookings.getEventBookings = async function (req, res) {
     //need to loop over a users roles and assemble the data they are allowed to see
     const event = await db.event.findOne({where: {id: {[Op.eq]: req.params.eventId}}});
 
-    const flat = await getBookingsAndCombineScopes(req.user, event, 0)
+    const flat = await getBookingsAndCombineScopes(req.user, event)
 
     res.json({bookings: flat});
 
@@ -360,15 +360,10 @@ const getBookingAndCombineScopes = async function (user, booking) {
     return [obj]
 }
 
-const getBookingsAndCombineScopes = async function (user, event, offset=0) {
+const getBookingsAndCombineScopes = async function (user, event) {
 
     const scopes = getUserScopes(user, event);
-    const results = []
-
-    for(const scope of scopes) {
-        const result = await db.booking.scope(scope).findAll({limit: 20, offset: offset});
-        results.push(result)
-    }
+    const results = await Promise.all(scopes.map(s => db.booking.scope(s).findAll()));
 
     const obj = results.filter(r => r).reduce((a, c) => {
 
