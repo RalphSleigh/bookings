885f4f6b9e3d75979766b28b49fa83f97a3705ef
Ralph <ralph.sleigh@gmail.com>
2022-05-03 13:48:49 +0100

    DOnt update lock

--- migrations/14-add-bookings-constraint.js
+++ migrations/14-add-bookings-constraint.js
@@ -2,3 +2,5 @@ module.exports = {
     up:   (queryInterface, DataTypes) => queryInterface.addConstraint('bookings',{type: 'UNIQUE', fields: ['userId', 'eventId']}),
     down: (queryInterface, DataTypes) => Promise.resolve()
 };
+
+//  --max-old-space-size=1536
