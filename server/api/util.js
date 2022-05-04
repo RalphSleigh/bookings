e53a77838f19907833873c84c0df088ab1124319
Ralph <ralph.sleigh@gmail.com>
2022-05-04 15:18:49 +0100

    Hairy change to fix a bug

--- server/api/util.js
+++ server/api/util.js
@@ -6,7 +6,7 @@ module.exports.updateAssociation = (instance, key, Association, values) => {
 	//delete no longer present
 	ops = [...ops, ...instance[key].filter(p => !values.find(q => q.id === p.id)).map(p => p.destroy())];
 	//update existing
-	ops = [...ops, ...values.map(p => Association.findOne({ where: { id: p.id } }).then(q => q ? q.update(p) : null))];
+	ops = [...ops, ...values.filter(p => p.id).map(p => Association.findOne({ where: { id: p.id } }).then(q => q ? q.update(p) : null))];
 	//add new ones
 	ops = [...ops, ...values.filter(p => !p.id).map(p => {
         p[Association.associations[instance.constructor.name].foreignKey] = instance.id;
